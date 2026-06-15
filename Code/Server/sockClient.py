2e8687cbf31393e31db9d2d5d0e9c2459de13a7a
Terhi <tkoskenv@uef.fi>
2026-06-15 10:51:48 +0300

    juups

--- Code/Server/sockClient.py
+++ Code/Server/sockClient.py
@@ -2,7 +2,7 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def connectSock(ip = "10.239.227.76", port = 8702):
+def connectSock(ip = "10.239.227.76", port = 6767):
     s.connect((ip, port))
     
 def sendMessage(msg):
