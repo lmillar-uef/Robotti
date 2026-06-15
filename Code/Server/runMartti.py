0ac386f656093aad9884b6e222659ea14611a27b
lmillar-uef <lmillar@uef.fi>
2026-06-15 12:48:52 +0300

    threading

--- Code/Server/runMartti.py
+++ Code/Server/runMartti.py
@@ -10,11 +10,41 @@ from threading import Thread
 import time
 import car
 
+
+
+
 connected = False
 speaker=speakerGpio.Speaker()
 servo = servo.Servo()
 servo0_home = 90
 servo1_home = 90
+
+
+
+def excecuteCommand(msg):
+    if msg == "chase":
+		ledi.theaterChaseRainbow()
+	if msg == "autobots":
+		 car.test_car_sonic()
+	if msg == "I love you":
+		ledi.colorWipe((255, 0, 0))
+	if msg == "ping":
+	    sock.sendMessage("pong")
+	if msg == "play":	
+		speaker.playFrequency("A4")
+	if msg == "servo":	
+		servo.setServoAngle('0', 50)
+		servo.setServoAngle('1', 70) 
+		time.sleep(2)
+		servo.setServoAngle('0', servo0_home)
+		servo.setServoAngle('1', servo1_home)  
+	if msg == "stop":		
+		speaker.stop()
+	if msg == "kys":
+	    sock.shutDown()
+	    break
+
+excecute = Thread(target = excecuteCommand, args = ())
 
 
 while connected == False:
@@ -28,31 +58,9 @@ while True:
 	print("Listening...")
 	msg = sock.listenSock()
 	print(msg)
-	if msg == "chase":
-		ledi.theaterChaseRainbow()
-	if msg == "ultra":
-		 car.test_car_sonic()
-	if msg == "I love you":
-		ledi.colorWipe((255, 0, 0))
-	if msg == "ping":
-	    sock.sendMessage("pong")
-	if msg == "play":	
-		speaker.playFrequency("A4")
-	if msg == "autobots":	
-		servo.setServoAngle('0', 50)
-<<<<<<< HEAD
-		servo.setServoAngle('1',70)   
-=======
-		servo.setServoAngle('1', 70) 
-		time.sleep(2)
-		servo.setServoAngle('0', servo0_home)
-		servo.setServoAngle('1', servo1_home)  
->>>>>>> refs/remotes/origin/master
-	if msg == "stop":		
-		speaker.stop()
-	if msg == "kys":
-	    sock.shutDown()
-	    break
+	excecute = Thread(target = excecuteCommand, args = (msg,))
+	excecute.start()
+	
 
 	
 
