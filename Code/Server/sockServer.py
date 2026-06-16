6ea0ae0b1677bfde9b0abe0e2db56396dbf32165
lmillar-uef <lmillar@uef.fi>
2026-06-16 15:19:59 +0300

    muutoksia sockettiin

--- Code/Server/sockServer.py
+++ Code/Server/sockServer.py
@@ -5,33 +5,42 @@ from threading import Thread
 
 def connect():
     clientSocket, address = s.accept()
-    clientSocket.send(bytes("connected", "utf-8"))
+    clientSocket.sendall(bytes("connected\n", "utf-8"))
     return clientSocket, address
     
-def background_controller():
-    msg = input("Send message:")
-    clientSocket.sendall(bytes(msg, "utf-8"))
-    Timer(1, background_controller).start()
+def send():
+    while True:
+        msg = input("Send message:") + "\n"
+        clientSocket.sendall(bytes(msg, "utf-8"))
 
-def listen():
-    #process msg
-    print(clientSocket.recv(1024).decode("utf-8"))
-    Timer(1, listen).start()
+def listen(clientSocket):
+    while True:
+        data = clientSocket.recv(1024)
+        if not data:
+            print("Client disconnected.")
+            break
+        print("Recieved:", data.decode("utf-8"))
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('0.0.0.0', 6751))
+s.listen(1)
 
-s.bind(('0.0.0.0', 6719))
-s.listen()
 print('Server is now running.')
 
-#clientSocket, address = connect()
-#listener_thread = Thread(target=listen, daemon=True)
+clientSocket, address = connect()
+print(f"Connection from {address} has been established.")
+
+listener_thread = Thread(target=listen, args=(clientSocket,), daemon=True)
+
     
 try:
+    listener_thread.start()
+    send(clientSocket)
     while True:
         clientSocket, address = connect()
-        print(f"Connection from {address} has been established.")
+        
         #listener_thread.start()
         background_controller()
         listen()
