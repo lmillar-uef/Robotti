825adaf2f17894f323c12c6fcd822e6079ddce58
lmillar-uef <lmillar@uef.fi>
2026-06-19 12:59:10 +0300

    placeholder

--- Code/Server/dance.py
+++ Code/Server/dance.py
@@ -1,11 +1,11 @@
 dance1 = [[],]
 
 def dance_mode(self, index):
+    pass
 
 def move(motor, direction, duration):
     if direction == "":
         pass
-    gergd
     
     time.sleep(duration)
     motor.set_model(0, 0)
